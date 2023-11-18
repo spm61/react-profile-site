af362b7653bc4bea48ad7e682956557136636e54
spmoroney <spmoroney@att.net>
2023-11-18 11:25:16 -0500

    Add first component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="font-Poppins bg-ghostWhite">
+      <Navbar />
+      <Home />
+      <Projects />
+      <Services />
+      <Blog />
+      <Testimonials />
+      <Contact />
+      <Footer />
+      <ScrollToTop />
+    </div>
+  );
 }
 
-export default App
+export default App;
