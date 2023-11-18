1752d26dd448c618b5a8f9d60a8bfe66564c79f6
spmoroney <spmoroney@att.net>
2023-11-18 15:09:14 -0500

    alter router base to match rest of app.

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     open: true
   },
-  base: "/react-portfolio/"
+  base: "/"
 })
